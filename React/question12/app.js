0172ea6141c298015eeba1b403ac640355df78ee
Aviral Singh Pal <aviralpal00007@gmail.com>
2023-06-06 22:54:27 +0530

    Create app.js

---
+++ React/question12/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CounterApp from './CounterApp';
+
+function App() {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <CounterApp />
+    </div>
+  );
+}
+
+export default App;
