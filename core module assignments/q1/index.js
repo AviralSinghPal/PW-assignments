4385662969c52f9cd32714fbb015e7008b0847ae
Your Name <you@example.com>
2023-07-09 23:45:13 +0530

    question1 completed

---
+++ core module assignments/q1/index.js
@@ -0,0 +1,8 @@
+
+const a = document.getElementsByClassName("box")[0];
+
+a.addEventListener('click', ()=>{
+    a.style.backgroundColor = 'yellow';
+    a.style.width = '600px';
+    a.style.height = '600px';
+})
